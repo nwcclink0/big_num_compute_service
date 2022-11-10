de6938c312088afc6c5b8618650bf7663715d7dd
yuantingwei <yuantingwei@pm.me>
2022-11-11 06:26:06 +0800

    feat: usage hint

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"big_num_compute_service/config"
 	"big_num_compute_service/service"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
@@ -12,7 +14,8 @@ func main() {
 		configFile string
 	)
 	var err error
-
+	flag.Usage = usage
+	flag.Parse()
 	flag.StringVar(&configFile, "c", "", "Configuration file path")
 	service.BigNumComputeConf, err = config.LoadConf(configFile)
 	if err != nil {
@@ -27,4 +30,15 @@ func main() {
 	service.InitDb()
 	service.InitWorker(service.BigNumComputeConf.Core.WorkerNum, service.BigNumComputeConf.Core.QueueNum)
 	service.Run()
+}
+
+var usageStr = `
+Usage: big_number_compute service [options]
+Server Options:
+	-c, --config <file>
+`
+
+func usage() {
+	fmt.Printf("%s\n", usageStr)
+	os.Exit(0)
 }
