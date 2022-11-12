5cdc5f9f6d54d9fb926dcab35f73773e60050ba0
yuantingwei <yuantingwei@pm.me>
2022-11-12 10:51:30 +0800

    chore: build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TARGET=$1
+GO_OSARCH=
+GO_BUILD_TAGS=
+OUTPUT_SUBDIR=
+GO_BUILD_TAGS=""
+if [ "$TARGET" = "lb" ]; then
+  export GOOS=linux
+  export GOARCH=amd64
+  go build -v -o="./load_balancer/service/big_num_compute_service"
+  docker build -t yuantingwei/big_num_compute_service:v0.1 -f ./load_balancer/service/Dockerfile ./load_balancer/service
+  cd load_balancer || echo "load_balancer don't exist" exit
+  docker-compose build
+  docker-compose up --scale big_num_compute_service=2
+elif [ "$TARGET" = "native" ]; then
+  go build -v
+else
+  echo "./build [load_balancer/native]"
+fi
